4dd81ffe1adadcb296c0ef321e0b9b4ea8d032c3
meloegel <meloegel@gmail.com>
2022-05-08 21:32:09 -0400

    inital commit

---
+++ src/main/kotlin/com/mloegel/golfTracker/GolfTrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.mloegel.golfTracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GolfTrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication<GolfTrackerApplication>(*args)
+}
