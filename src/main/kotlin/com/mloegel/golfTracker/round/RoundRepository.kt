f0a12aaea1ede3e3ded9edff73e51f35a2dc25ec
meloegel <meloegel@gmail.com>
2022-05-10 22:24:14 -0400

    Added findAllRounds, findRoundById and findRoundsByCourseName to Round service and repository

--- src/main/kotlin/com/mloegel/golfTracker/round/RoundRepository.kt
+++ src/main/kotlin/com/mloegel/golfTracker/round/RoundRepository.kt
@@ -3,4 +3,7 @@ package com.mloegel.golfTracker.round
 import org.springframework.data.repository.CrudRepository
 
 interface RoundRepository : CrudRepository<Round, String> {
+    fun findByRoundid(roundid: Int): Round
+
+    fun findRoundsByCourseName(courseName: String): List<Round>
 }
--- src/main/kotlin/com/mloegel/golfTracker/round/RoundService.kt
+++ src/main/kotlin/com/mloegel/golfTracker/round/RoundService.kt
@@ -5,4 +5,9 @@ import org.springframework.stereotype.Service
 
 @Service
 class RoundService(val db: RoundRepository) {
+    fun findAllRounds(): MutableIterable<Round> = db.findAll()
+
+    fun findRoundById(roundid: Int): Round = db.findByRoundid(roundid)
+
+    fun findRoundsByCourseName(courseName: String): List<Round> = db.findRoundsByCourseName(courseName)
 }
