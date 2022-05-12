d2e7f9b94969761272313412bf25c7e5e7ff9120
meloegel <meloegel@gmail.com>
2022-05-11 23:32:57 -0400

    Added findAllRounds to Round controller

--- src/main/kotlin/com/mloegel/golfTracker/round/RoundController.kt
+++ src/main/kotlin/com/mloegel/golfTracker/round/RoundController.kt
@@ -1,4 +1,11 @@
 package com.mloegel.golfTracker.round
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class RoundController(val service: RoundService) {
+    @GetMapping("/rounds")
+    fun getAllRounds(): MutableIterable<Round> = service.findAllRounds()
+
 }
