0eaa92b08fcc91d7db405b82689a08a906104bf9
meloegel <meloegel@gmail.com>
2022-05-12 22:27:58 -0400

    Added getRoundByRoundid to Round controller

--- src/main/kotlin/com/mloegel/golfTracker/round/RoundController.kt
+++ src/main/kotlin/com/mloegel/golfTracker/round/RoundController.kt
@@ -1,11 +1,22 @@
 package com.mloegel.golfTracker.round
 
+import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class RoundController(val service: RoundService) {
     @GetMapping("/rounds")
     fun getAllRounds(): MutableIterable<Round> = service.findAllRounds()
+
+    @GetMapping("/rounds/round/{roundid}")
+    fun getRoundByRoundid(@PathVariable roundid: Int): Round {
+        try {
+            return service.findRoundById(roundid)
+        }catch (exception: EmptyResultDataAccessException) {
+            throw Exception("Round with id $roundid not found! Exception: $exception")
+        }
+    }
 
 }
