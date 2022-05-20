93adcdaa4562e5d5423a23489ca2b65419901a81
meloegel <meloegel@gmail.com>
2022-05-19 22:32:58 -0400

    Completed hole data class

--- src/main/kotlin/com/mloegel/golfTracker/hole/Hole.kt
+++ src/main/kotlin/com/mloegel/golfTracker/hole/Hole.kt
@@ -1,10 +1,15 @@
 package com.mloegel.golfTracker.hole
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.mloegel.golfTracker.round.Round
 import javax.persistence.Entity
 import org.springframework.data.relational.core.mapping.Table
+import javax.persistence.Column
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
 
 
 @Entity
@@ -14,5 +19,20 @@ data class Hole(
     @GeneratedValue(strategy = GenerationType.AUTO)
     val holeid: Int?,
 
+    @Column(nullable = false)
+    val par: Int,
 
+    @Column(nullable = false)
+    val score: Int,
+
+    @Column
+    val putts: Int,
+
+    @Column
+    val notes: String,
+
+    @ManyToOne
+    @JoinColumn(name = "roundid", nullable = false)
+    @JsonIgnoreProperties(value = ["date", "totalScore", "courseName", "description", "score"], allowSetters = true)
+    var round: Round? = null
 )
