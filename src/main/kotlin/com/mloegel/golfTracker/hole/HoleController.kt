27bbd9bd1ab714721bc0f047ddb50592627ac7db
meloegel <meloegel@gmail.com>
2022-05-23 21:25:20 -0400

    Added postHole

--- src/main/kotlin/com/mloegel/golfTracker/hole/HoleController.kt
+++ src/main/kotlin/com/mloegel/golfTracker/hole/HoleController.kt
@@ -1,4 +1,14 @@
 package com.mloegel.golfTracker.hole
 
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class HoleController(val service: HoleService) {
+    @PostMapping("/hole")
+    fun postHole(@RequestBody hole: Hole): Hole {
+        return service.postHole(hole)
+    }
+
 }
