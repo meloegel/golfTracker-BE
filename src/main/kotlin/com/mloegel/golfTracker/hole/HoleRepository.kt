e7d2325292f7759e0461555463b95528fb708935
meloegel <meloegel@gmail.com>
2022-05-23 22:50:24 -0400

    Added findByHoleid

--- src/main/kotlin/com/mloegel/golfTracker/hole/HoleRepository.kt
+++ src/main/kotlin/com/mloegel/golfTracker/hole/HoleRepository.kt
@@ -3,4 +3,7 @@ package com.mloegel.golfTracker.hole
 import org.springframework.data.repository.CrudRepository
 
 interface HoleRepository : CrudRepository<Hole, String>{
+
+    fun findByHoleid(holeid: Int): Hole
+
 }
--- src/main/kotlin/com/mloegel/golfTracker/hole/HoleService.kt
+++ src/main/kotlin/com/mloegel/golfTracker/hole/HoleService.kt
@@ -7,6 +7,8 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class HoleService(val db: HoleRepository) {
 
+    fun findByHoleid(holeid: Int): Hole = db.findByHoleid(holeid)
+
     @Transactional
     fun postHole(hole: Hole) = db.save(hole)
 
