4efe46f834527ec4c2bea6ceeedbeed39e9c7dbe
meloegel <meloegel@gmail.com>
2022-05-30 23:35:03 -0400

    added user data class

---
+++ src/main/kotlin/com/mloegel/golfTracker/user/User.kt
@@ -0,0 +1,28 @@
+package com.mloegel.golfTracker.user
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.mloegel.golfTracker.round.Round
+import org.springframework.data.relational.core.mapping.Table
+import javax.persistence.*
+
+
+@Entity
+@Table("USERS")
+data class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var userid: Int?,
+
+    @Column(nullable = false, unique = true)
+    var username: String,
+
+    @Column(nullable = false)
+    var password: String,
+
+    @Column(nullable = false, unique = true)
+    var email: String,
+
+    @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], orphanRemoval = true)
+    @JsonIgnoreProperties(value = ["user"], allowSetters = true)
+    var Rounds: List<Round>? = ArrayList<Round>()
+)
