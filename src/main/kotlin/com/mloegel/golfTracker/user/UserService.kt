c4eb2c6f4c55a9ac2cbdf2b4bfa44ac596e9ab6d
meloegel <meloegel@gmail.com>
2022-06-01 22:09:03 -0400

    added postUser and deleteUser to user service

--- src/main/kotlin/com/mloegel/golfTracker/user/UserService.kt
+++ src/main/kotlin/com/mloegel/golfTracker/user/UserService.kt
@@ -1,6 +1,7 @@
 package com.mloegel.golfTracker.user
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
 
 
@@ -14,4 +15,13 @@ class UserService(val db: UserRepository) {
     fun findByUsername(username: String): User = db.findByUsername(username)
 
     fun searchByUsername(username: String): List<User> = db.findByUsernameContainingIgnoreCase(username)
+
+    @Transactional
+    fun postUser(user: User) = db.save(user)
+
+    @Transactional
+    fun deleteUser(user: User) = db.delete(user)
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    fun deleteAll() = db.deleteAll()
 }
