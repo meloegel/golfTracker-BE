3ca73571738605433fb34d7b42225b91c073a6fb
meloegel <meloegel@gmail.com>
2022-06-06 23:34:45 -0400

    added login to UserController

--- src/main/kotlin/com/mloegel/golfTracker/user/UserController.kt
+++ src/main/kotlin/com/mloegel/golfTracker/user/UserController.kt
@@ -1,12 +1,7 @@
 package com.mloegel.golfTracker.user
 
 import org.springframework.dao.EmptyResultDataAccessException
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class UserController(val service: UserService) {
@@ -39,14 +34,24 @@ class UserController(val service: UserService) {
         return service.searchByUsername(username)
     }
 
-@DeleteMapping("/user/{userid}")
-fun deleteUser(@PathVariable userid: Int) {
-    try {
-        val user = service.findByUserid(userid)
-        return service.deleteUser(user)
-    } catch (exception: EmptyResultDataAccessException) {
-        throw Exception("User with userid $userid not found! Exception: $exception")
+    @GetMapping("/login")
+    fun login(@RequestBody loginInfo: Login): String {
+        return service.login(loginInfo.username, loginInfo.password)
     }
-}
+
+    @DeleteMapping("/user/{userid}")
+    fun deleteUser(@PathVariable userid: Int) {
+        try {
+            val user = service.findByUserid(userid)
+            return service.deleteUser(user)
+        } catch (exception: EmptyResultDataAccessException) {
+            throw Exception("User with userid $userid not found! Exception: $exception")
+        }
+    }
 
 }
+
+data class Login(
+    val username: String,
+    val password: String
+)
